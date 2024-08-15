c66c828c8f15a3cc74b5d85038b7888a06ab5b4e
melnyk-anton <gramazeka2222@gmail.com>
2024-08-15 18:00:20 +0300

    kinda connected Udio

--- script2.js
+++ script2.js
@@ -37,8 +37,25 @@ document.getElementById('fetchVideoButton').addEventListener('click', async () =
   }
 
 });
-document.getElementById('moveToThirdStep').addEventListener('click', async () => {
-  window.location = 'thirdStep.html';
+
+
+document.getElementById('generateAudio').addEventListener('click', async () => {
+  try {
+    const response = await fetch('https://melnyk-anton--example-lifecycle-web2-get-audio-dev.modal.run', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ request: 'get_audio', prompt: "hello"})
+    });
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error)
+  {
+    console.error('Error fetching video:', error);
+  }
+
 });
-
-
