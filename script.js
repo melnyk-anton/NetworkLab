82fcce6b5dffab95a18297336a1443c9d23f732c
melnyk-anton <gramazeka2222@gmail.com>
2024-08-17 04:04:47 +0300

    hopefully last commit

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-document.getElementById('fetchVideoButton').addEventListener('click', async () => {
+const generateVideoButton = document.getElementById('fetchVideoButton');
+function changeToLoading() {
+    const generateVideoDiv = document.getElementById('containerForButton');
+    generateVideoDiv.innerHTML = '<button id="fetchVideoButton" class="fetchVideoButton" style="background-color:Red;"><span class="loader">L &nbsp; ading</span></button>';
+}
+function changeToRegenerate() {
+    const generateVideoDiv = document.getElementById('containerForButton');
+    generateVideoDiv.innerHTML = '<button id="fetchVideoButton" class="fetchVideoButton"><i class="fa fa-refresh" style="font-size:24px"></i> Regenerate</button>';
+}
+generateVideoButton.addEventListener('click', async () => {
     try {
+        changeToLoading();
         const videoElement = document.getElementById('videoElement');
-        videoElement.src = "https://static.videezy.com/system/resources/previews/000/037/474/original/circle_loading.mp4";
-        document.getElementById('videoContainer').style.display = 'block';
-        videoElement.play();
         const textField = document.getElementById('text');
         const response = await fetch('https://hook.eu2.make.com/1gsmoulbtrw9hwpdi7679oo8cmucyxra', {
             method: 'POST',
@@ -12,17 +19,16 @@ document.getElementById('fetchVideoButton').addEventListener('click', async () =
             },
             body: JSON.stringify({ request: 'getVideo', content: textField.value})
         });
-
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-
+        console.log(111);
         const data = await response.json();
         if (data.url) {
+            changeToRegenerate();
+            console.log(222);
             videoElement.loop = false;
             videoElement.src = data.url;
             document.getElementById('videoContainer').style.display = 'block';
             videoElement.play();
+            console.log(333);
         } else {
             console.error('No video URL returned');
         }
